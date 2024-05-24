82a6a27ff3ee0b7af343d1fd785f4ef6dbc3d2e1
BrightDistribution <matei.ionescu@bebright.ro>
2024-05-24 10:32:03 +0300

    buton mai mare

--- src/components/ButtonBg.jsx
+++ src/components/ButtonBg.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ButtonBg(props) {
   return (
     <div>
-        <button className='buttonS w-[140px] h-[60px] text-xl m-[20px] rounded-2xl'>{props.text}</button>
+        <button className='buttonS w-[160px] h-[80px] text-xl m-[20px] rounded-2xl'>{props.text}</button>
     </div>
   )
 }
